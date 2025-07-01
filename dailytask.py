79351c83adf619c8b951ebc5f8cdcc56fd419ecc
Sultan <iamsultanyousuf@gmail.com>
2025-07-01 21:29:13 +0530

    Sixth

---
+++ dailytask.py
@@ -0,0 +1,98 @@
+# correct = "python123"
+# password = ""
+# while password != correct:
+#     password = input("Enter password: ")
+# else:
+#     print("Correct password entered.")
+
+# num=[1,2,3,4,5,6,7,8,9]
+# target=10
+# for i in num:
+#     if i==target:
+#         print("Number found!!")
+#         break
+# else:
+#     print("Number not found!!")
+
+# secret = 69 
+# attempts = 3  
+# for i in range(attempts):
+#     user = int(input("Guess the secret number: "))
+#     if user == secret:
+#         print("Congratulations! You guessed the correct number.")
+#         break 
+# else:
+#     print("Game over! You failed to guess the number.")   
+
+# for i in range(4):
+#     for j in range(6):
+#         print("* ",end=" ")
+#     print() 
+
+# n=5 
+# for i in range(n):
+#     for j in range(n):
+#         if i==0 or i==n-1 or j==0 or j==n-1:
+#             print("*", end=" ")
+#         else:
+#             print(" ", end=" ")
+#     print()   
+
+# for i in range(5,0,-1):
+#     for j in range(1, i + 1):
+#         print(j,end=" ")
+#     print()
+
+# def calculate(price,tax=5):
+#     return price + (price*tax/100)
+# print(calculate(100))
+
+# list1=["soap","detergent"]
+# def add(item):
+#     global list1
+#     list1.append(item)
+# add("shampoo")
+# print(list1)
+
+# text = 'Python Programming is fun!'
+# vowels = []
+# for i in text:
+#     if i.lower() in 'aeiou':
+#         vowels.append(i)
+# print(vowels)
+
+# li=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+# odd = [num for num in li if num % 2 != 0]
+# print(odd)
+
+# week = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+ 
+# print(week[-1]) 
+
+# print(week[1:4])  
+
+# print(week.index("Wednesday"))  
+
+# student = ["Momi", "Rashi", "Sulu", "Max", "Ronnie", "Pessi"]
+# setlist= set(student)
+# print(setlist)
+
+# set1 = {1, 2, 3, 4, 5, 6}
+# set2 = {4, 5, 6, 7, 8, 9}
+
+# union_set = set1 | set2
+# intersection_set = set1 & set2
+# difference_set = set1 - set2
+
+# print(union_set)
+# print(intersection_set)
+# print(difference_set)
+
+# subset_check = {4, 5}.issubset(set1)
+# print(subset_check)
+
+
+
+
+
+        
