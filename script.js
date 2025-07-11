c1a7b1a349f45c98c9181fb0b3d189cd80aa0ae8
Sultan <iamsultanyousuf@gmail.com>
2025-07-11 12:01:51 +0530

    sixteenth

--- script.js
+++ script.js
@@ -1,7 +1,99 @@
 // console.log("All i want for christmas is food");
 
-var name = "john";
-let age = 25;
-const isStudent = true;
+// var name = "John";
+// let age = 25;
+// const isStudent = true;
 
-console.log(name,age);
+// console.log(name,age);
+// <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+
+// var name = "John";
+// let age = 25;
+
+// console.log(`My name is ${name} and I am ${age} years old`);
+
+// <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><> 
+
+// let age =69;
+// if(age<18){
+//     console.log("You are a minor.");
+// }else if(age>=18 && age<60){
+//     console.log("You are an adult.");
+// }else{
+//     console.log("You are a senior citizen.")
+// }
+
+// <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+
+// let num = 0;
+// if(num>0){
+//     console.log("The number is positive.");
+// }else if(num<0){
+//     console.log("The  number is negative.");
+// }else{
+//     console.log("It is zero.")
+// }
+
+// <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+
+// let day=3;
+// switch(day){
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//         console.log("Tuesday");
+//         break;
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+//     default:
+//         console.log("Invalid day");
+// }
+
+// <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+
+// let char="a";
+// switch(char){
+//     case "a":
+//     case "e":
+//     case "i":
+//     case "o":
+//     case "u":
+//         console.log("Its a vowel");
+//         break;
+//     default:
+//         console.log("Its not a vowel");
+// }
+
+// <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+
+let mark= 99;
+switch(mark){
+    case 100:
+        console.log("O grade");
+        break;
+    case (mark>=90):
+        console.log("A+ grade");
+        break;
+    case mark >= 80 :
+        console.log("A grade");
+        break;
+    case mark >=70:
+        console.log("B+ grade");
+        break;
+    case mark >= 60:
+        console.log("B grade");
+        break;
+    case mark >= 50 :
+        console.log("C+ grade");
+        break;
+    case mark >=40:
+        console.log("C grade");
+        break;
+    case mark < 40:
+        console.log("Failed");
+        break;
+    default:
+        console.log("Invalid mark");
+}
