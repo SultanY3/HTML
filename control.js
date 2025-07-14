e997cf4d30ad7085e394e85ea64b25bf290a921e
Sultan <iamsultanyousuf@gmail.com>
2025-07-14 11:45:18 +0530

    19th

---
+++ control.js
@@ -0,0 +1,39 @@
+// For loop
+// for (let i = 0; i < 5; i++) {
+//     console.log(i);
+// }
+
+// While loop
+// let i = 0;
+// while (i < 5) {
+//     console.log(i);
+//     i++;
+// }
+
+// do...while loop
+// let i = 0;
+// do {
+//     console.log(i);
+//     i++;
+// } while (i < 5);
+
+// break
+// for (let i = 0; i < 10; i++) {
+//     if (i === 5) {
+//         break;
+//     }
+//     console.log(i);
+// }
+
+// Continue
+// for (let i = 1; i < 10; i++) {
+//     if (i === 5) {
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+
+
+
+
