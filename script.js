5c9516d4e7038a981a02694c4085fcadb782a49c
Sultan <iamsultanyousuf@gmail.com>
2025-07-14 12:03:02 +0530

    Nineteenth

--- script.js
+++ script.js
@@ -102,9 +102,9 @@
 
 // let num = 5;
 // let fact = 1;
-// for(let i=1;i<=Number;i++){
-//     fact=fact*i
+// for (let i = 1; i <= num; i++) {
+//     fact *= i;
 // }
-// console.log(fact)
+// console.log(fact);
 
 // <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
