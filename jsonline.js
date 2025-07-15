1cd09061bddd518d31af0b996f725ca317cf9f30
Sultan <iamsultanyousuf@gmail.com>
2025-07-15 22:44:16 +0530

    twentieth

---
+++ jsonline.js
@@ -0,0 +1,25 @@
+// write a program to print only fruits with more than 5 letters using loop
+
+// let fruits = ['Apple', 'Pineapple', 'Peach', 'Grapes', 'Strawberry'];
+// for (let i = 0; i < fruits.length; i++) {
+//     if (fruits[i].length > 5) {
+//         console.log(fruits[i])
+//     }
+// }
+
+//Write a program to find the name of the youngest employee in an array of objects
+
+// let employee = [
+//     { name: 'Shein', age: 28 },
+//     { name: 'Priya', age: 38 },
+//     { name: 'Merlyn', age: 20 },
+// ];
+// let a = employee[0];
+// // console.log(a);
+// for (let i = 0; i < employee.length; i++) {
+//     if (employee[i].age < a.age) {
+//         a = employee[i];
+
+//     }
+// }
+// console.log(a);
