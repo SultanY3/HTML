bae4c6480d44638279f61a50092529998aa334b9
Sultan <iamsultanyousuf@gmail.com>
2025-07-21 12:00:00 +0530

    26th

--- classwork23/7.js
+++ classwork23/7.js
@@ -38,10 +38,10 @@
 
 // <><><><><>Using the fetch API<><><><><>
 
-fetch("https://jsonplaceholder.typicode.com/posts/")
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.error("Error:", error));
+// fetch("https://jsonplaceholder.typicode.com/posts/")
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+//     .catch(error => console.error("Error:", error));
 
 
 
